cdb81d4f81f62641cb1bae9c62de7aa16510a4bd
ANIL KESKİN <anil.keskin@vestel.com.tr>
2023-04-13 15:47:49 +0300

    Global Exception Middleware

---
+++ GlobalExceptionMiddleware/Controllers/ExceptionTestController.cs
@@ -0,0 +1,34 @@
+﻿using GlobalExceptionMiddleware.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GlobalExceptionMiddleware.Controllers
+{
+    [ApiController]
+    public class ExceptionTestController : ControllerBase
+    {
+        [HttpGet]
+        [Route("BadRequestException")]
+        public IActionResult BadRequestException()
+        {
+            throw new BadRequestException("Example BadRequestException - 400 - Client Side Error");
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("NotFoundException")]
+        public IActionResult NotFoundException()
+        {
+            throw new NotFoundException("Example NotFoundException - 404 - User not found.");
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("InternalServerError")]
+        public IActionResult InternalServerError()
+        {
+            int number1 = 50, number2 = 0;
+            int result = number1/number2;
+            return Ok();
+        }
+    }
+}
---
+++ GlobalExceptionMiddleware/Middlewares/GlobalExceptionHandler.cs
@@ -0,0 +1,48 @@
+﻿using GlobalExceptionMiddleware.Exceptions;
+using System.Net;
+using System.Text.Json;
+
+namespace GlobalExceptionMiddleware.Middlewares
+{
+    public class GlobalExceptionHandler
+    {
+        private readonly RequestDelegate _next;
+        public GlobalExceptionHandler(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next.Invoke(context);
+            }
+            catch (Exception error)
+            {
+                await HandleExceptionAsync(context, error);
+            }
+        }
+        private static async Task HandleExceptionAsync(HttpContext context, Exception error)
+        {
+            var response = context.Response;
+            response.ContentType = "application/json";
+            switch (error)
+            {
+                case BadRequestException:
+                    response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    break;
+                case NotFoundException:
+                    response.StatusCode = (int)HttpStatusCode.NotFound;
+                    break;
+                case UnauthorizedException:
+                    response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                    break;
+                default:
+                    response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    break;
+            }
+            var result = JsonSerializer.Serialize(new { message = error?.Message });
+            await response.WriteAsync(result);
+        }
+    }
+}
